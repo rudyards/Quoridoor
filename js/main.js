3c2cf77377afa3dd03c8dd5280192b97ca8b81b6
rudyards <rudy@mightycheese.com>
2019-05-06 14:37:21 -0700

    Implement basic move logic
    Reminder needs to be prettier, currently the move logic is brute force, so could benefit from changing.

--- js/main.js
+++ js/main.js
@@ -6,18 +6,66 @@ const PLAYER = {
 }
 
 /*----- app's state (variables) -----*/
-var board, winner, turn
+var board, winner, turn;
 
 /*----- cached element references -----*/ 
 const msgEl = document.getElementById('msg');
+const remindEl = document.getElementById('reminder');
 const boardEl = document.querySelectorAll('td');
 
 /*----- event listeners -----*/ 
-
+document.querySelector('table').addEventListener('click', handleClick);
 
 /*----- functions -----*/
 initialize();
 
+function handleClick(evt){
+    if (evt.target.id[1] % 1 === 1) {
+        placeWall(evt.target);
+    } else {
+        movePlayer(evt.target);
+    }
+    render();
+}
+
+function movePlayer(target){
+    var location = target.id.split('r');
+    var row = parseInt(location[1]);
+    var col = parseInt(location[0].replace('c', ''));
+    // After we have the precise location of the target, such that we could look at the board and find it, now we
+    // make sure that the location is next ot the player.
+
+    // Since I can't think of an elegant way, I'm going to brute force this. Revisit later.
+    if (board[row+2][col] === turn){
+        board[row+2][col] = null;
+        board[row][col] = turn;
+        turn *= -1;
+        remindEl.textContent = "";
+    } else if (board[row-2][col] === turn){
+        board[row-2][col] = null;
+        board[row][col] = turn;
+        turn *= -1;
+        remindEl.textContent = "";
+    } else if (board[row][col+2] === turn){
+        board[row][col+2] = null;
+        board[row][col] = turn;
+        turn *= -1;
+        remindEl.textContent = "";
+    } else if (board[row][col-2] === turn){
+        board[row][col-2] = null;
+        board[row][col] = turn;
+        turn *= -1;
+        remindEl.textContent = ;
+    } else {
+        remindEl.textContent = "That is too far away.";
+    }
+
+}
+
+function placeWall(target){
+    //Code for placing the wall correctly
+    //Don't render at the end, since this is only ever called in handleClick
+}
 
 function render(){
     msgEl.textContent = `${PLAYER[turn].toUpperCase()}'s Turn`;
